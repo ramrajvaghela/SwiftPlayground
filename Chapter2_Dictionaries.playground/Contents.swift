c833db1a193a4015a69b5eab2e02220fc265968d
ramrajvaghela <ramrajvaghela@gmail.com>
2022-02-25 16:55:04 +0530

    Test

---
+++ Chapter2_Dictionaries.playground/Contents.swift
@@ -0,0 +1,70 @@
+import UIKit
+print("Dictionaries")
+/*
+ You are given a dictionary code of type [String:String] which has values for all lowercase letters. The codedictionary represents a way to encode a message. For example if code["a"] = "z" and code["b"] = "x" the encoded version if "ababa" will be "zxzxz". You are also given a encodedMessage which contains only lowercase letters and spaces. Use the codedictionary to decode the message and print it. Write a Swift program to decode the message and print final output string
+ */
+
+var code = [
+"a" : "b",
+
+"b" : "c",
+
+"c" : "d",
+
+"d" : "e",
+
+"e" : "f",
+
+"f" : "g",
+
+"g" : "h",
+
+"h" : "i",
+
+"i" : "j",
+
+"j" : "k",
+
+"k" : "l",
+
+"l" : "m",
+
+"m" : "n",
+
+"n" : "o",
+
+"o" : "p",
+
+"p" : "q",
+
+"q" : "r",
+
+"r" : "s",
+
+"s" : "t",
+
+"t" : "u",
+
+"u" : "v",
+
+"v" : "w",
+
+"w" : "x",
+
+"x" : "y",
+
+"y" : "z",
+
+"z" : "a",
+]
+
+var encodedMessage = "uijt nfttbhf jt ibse up sfbe5"
+for i in encodedMessage {
+    if let key = code.first(where: {$0.value == String(i)})?.key {
+        print(key,terminator: "")
+    }
+    else if String(i) == " " {
+        print(" ",terminator: "")
+    }
+    
+}
